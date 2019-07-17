6cbce1232528ef7b130424c7a92c20b2d06de307
winbugsmx <51462197+winbugsmx@users.noreply.github.com>
2019-07-17 17:53:20 -0500

    Update index.php

--- src/index.php
+++ src/index.php
@@ -15,7 +15,7 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Demo Simple PHP App Demo</h1>
+                <h1>Esta es una demo en PHP para AWS ESC</h1>
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
